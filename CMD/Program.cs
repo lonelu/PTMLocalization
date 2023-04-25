7449e21d3c6398b114dcd68714bfbf82e65d6f02
dpolasky <dpolasky@umich.edu>
2023-04-25 16:18:27 -0400

    bump to 1.0.4

--- CMD/Program.cs
+++ CMD/Program.cs
@@ -14,7 +14,7 @@ namespace CMD
     public static class Program
     {
         private static CmdSettings CommandLineSettings;
-        private static readonly string version = "1.0.3";
+        private static readonly string version = "1.0.4";
 
         public static int Main(string[] args)
         {
